783b437716fdffa62052c49b69185e04684cc35e
Fernando Flores <emilianoflores07081@gmail.com>
2025-03-30 14:52:46 -0600

    update connection with API

--- public/src/scripts/index.js
+++ public/src/scripts/index.js
@@ -1,4 +1,4 @@
-const API_ENDPOINT = "http://54.145.242.237:3000/api/";
+const API_ENDPOINT = "https://apipawkeeper.fernando-e-flores.me/api/";
 
 document.addEventListener("DOMContentLoaded", () => {
   const id = window.location.pathname.split("/").pop();
